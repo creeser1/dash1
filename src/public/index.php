0519760b9ae42df39250322859a98e5e892efab9
Reeser <creeser@calstate.edu>
2016-09-06 15:37:47 -0700

    refined template

--- src/public/index.php
+++ src/public/index.php
@@ -130,6 +130,11 @@ $app->get('/test1', function ($request, $response, $args) {
 				'embed' => 'static/page/bublin/explanations.html'
 				],
 				[
+				'label' => 'Data Tables',
+				'anchor' => 'table',
+				'embed' => 'static/page/bublin/table.html'
+				],
+				[
 				'label' => 'Methodology',
 				'anchor' => 'method',
 				'embed' => 'static/page/bublin/method.html'
