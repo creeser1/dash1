5cc446a4c4484b2fc3e4ed12b8ef14efdbaef9fd
Reeser <creeser@calstate.edu>
2016-09-06 11:34:39 -0700

    added error pages

--- src/public/index.php
+++ src/public/index.php
@@ -8,11 +8,6 @@ spl_autoload_register(function ($classname) {
 });
 
 $config['displayErrorDetails'] = true;
-$config['db']['host']   = "localhost";
-$config['db']['user']   = "user";
-$config['db']['pass']   = "password";
-$config['db']['dbname'] = "exampleapp";
-
 
 $app = new \Slim\App(["settings" => $config]);
 $container = $app->getContainer();
@@ -48,12 +43,7 @@ $container['view'] = function ($container) {
     return $view;
 };
 
-$app->get('/hello/{name}', function ($request, $response, $args) {
-    return $this->view->render($response, 'profile.html', [
-        'name' => $args['name']
-    ]);
-})->setName('hello');
-
+/*some automated server monitoring expects this*/
 $app->get('/index.html', function ($request, $response, $args) {
     return $this->view->render($response, 'root.html', [
         'name' => 'test'
@@ -72,7 +62,6 @@ $app->get('/favicon.ico', function ($request, $response, $args) {
     ]);
 })->setName('favicon');
 
-
 $app->get('/bublin', function ($request, $response, $args) {
     return $this->view->render($response, 'bublin-template.html', [
         'name' => $args['name']
@@ -85,82 +74,21 @@ $app->get('/bublin5', function ($request, $response, $args) {
     ]);
 })->setName('bublin5');
 
-$app->get('/bublin4', function ($request, $response, $args) {
-    return $this->view->render($response, 'bublin-template4.html', [
-        'name' => $args['name']
-    ]);
-})->setName('bublin4');
-
 $app->get('/data/{dataset}', function ($request, $response, $args) {
 	$newResponse = $response->withHeader('Content-type', 'application/json');
     return $this->data->render($newResponse, $args['dataset'], [
         'name' => $args['dataset']
     ]);
 })->setName('dataset');
-/*
-$app->get('/', function (Request $request, Response $response) {
-    $response->getBody()->write("Hello");
-	//$this->logger->addInfo("Something interesting happened");
 
-    return $response;
-});
-*/
-/*$container['view'] = new \Slim\Views\PhpRenderer("../templates/");*/
-/*
-$container['db'] = function ($c) {
-    $db = $c['settings']['db'];
-    $pdo = new PDO("mysql:host=" . $db['host'] . ";dbname=" . $db['dbname'],
-        $db['user'], $db['pass']);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-    return $pdo;
-};
-/**/
-/*
-$app->get('/tickets', function (Request $request, Response $response) {
-    $this->logger->addInfo("Ticket list");
-    $mapper = new TicketMapper($this->db);
-    $tickets = $mapper->getTickets();
-
-    $response = $this->view->render($response, "tickets.phtml", ["tickets" => $tickets, "router" => $this->router]);
-    return $response;
-});
-
-$app->get('/ticket/new', function (Request $request, Response $response) {
-    $component_mapper = new ComponentMapper($this->db);
-    $components = $component_mapper->getComponents();
-    $response = $this->view->render($response, "ticketadd.phtml", ["components" => $components]);
-    return $response;
-});
-
-$app->post('/ticket/new', function (Request $request, Response $response) {
-    $data = $request->getParsedBody();
-    $ticket_data = [];
-    $ticket_data['title'] = filter_var($data['title'], FILTER_SANITIZE_STRING);
-    $ticket_data['description'] = filter_var($data['description'], FILTER_SANITIZE_STRING);
-
-    // work out the component
-    $component_id = (int)$data['component'];
-    $component_mapper = new ComponentMapper($this->db);
-    $component = $component_mapper->getComponentById($component_id);
-    $ticket_data['component'] = $component->getName();
-
-    $ticket = new TicketEntity($ticket_data);
-    $ticket_mapper = new TicketMapper($this->db);
-    $ticket_mapper->save($ticket);
-
-    $response = $response->withRedirect("/tickets");
-    return $response;
-});
-
-$app->get('/ticket/{id}', function (Request $request, Response $response, $args) {
-    $ticket_id = (int)$args['id'];
-    $mapper = new TicketMapper($this->db);
-    $ticket = $mapper->getTicketById($ticket_id);
-
-    $response = $this->view->render($response, "ticketdetail.phtml", ["ticket" => $ticket]);
-    return $response;
-})->setName('ticket-detail');
-/**/
+$app->get('/test1', function ($request, $response, $args) {
+    return $this->view->render($response, 'tpl_test1.html', [
+        'page' => [
+			'htmltitle' => 'Test Title',
+			'title' => 'CSU Something',
+			'style' => 'static/page/bublin/all_style.css'
+		]
+    ]);
+})->setName('test1');
 
 $app->run();
