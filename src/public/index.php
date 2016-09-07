d9e8bb41be3a7bc090907540846a1dbd549503d5
Reeser <creeser@calstate.edu>
2016-09-07 11:08:26 -0700

    revert index.php

--- src/public/index.php
+++ src/public/index.php
@@ -85,11 +85,9 @@ $app->get('/test1', function ($request, $response, $args) {
 	$this->logger->addInfo("routing to /test1");
 	$setup = new PageConfigurator('page_test1');
 	$this->logger->addInfo("got setup");
-	/*
 	$page = $setup->getSetup();
 	$this->logger->addInfo("got page");
 	$this->logger->addInfo($page);
-	*/
 	/*
     return $this->view->render($response, 'tpl_test1.html', [
         'page' => $page
