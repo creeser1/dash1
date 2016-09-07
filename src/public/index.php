76bea115f716a0c8a26aea5e0f3c58f602ed73b9
Reeser <creeser@calstate.edu>
2016-09-07 10:52:37 -0700

    added class for page construction

--- src/public/index.php
+++ src/public/index.php
@@ -87,21 +87,67 @@ $app->get('/data/{dataset}', function ($request, $response, $args) {
 })->setName('dataset');
 
 $app->get('/test1', function ($request, $response, $args) {
-	$this->logger->addInfo("routing to /test1");
-	/*
-	$setup = new PageConfigurator('page_test1');
-	*/
-	/*
-	$this->logger->addInfo("got setup");
-	$page = $setup->getSetup();
-	$this->logger->addInfo("got page");
-	$this->logger->addInfo($page);
-	*/
-	/*
+
     return $this->view->render($response, 'tpl_test1.html', [
-        'page' => $page
+        'page' => [
+			'htmltitle' => 'Test Title',
+			'title' => 'CSU Something',
+			'style' => 'static/page/bublin/all_style.css',
+			'controls' => [
+				[
+				'type' => 'select',
+				'id' => 'dataset_filter1',
+				'details' => [
+						[
+						'option_value' => 'ftf_6yr',
+						'option_text' => 'FTF 6yr Grad',
+						'isselected' => true
+						],
+						[
+						'option_value' => 'ftf_4yr',
+						'option_text' => 'FTF 4yr Grad'
+						],
+						[
+						'option_value' => 'tr_4yr',
+						'option_text' => 'TR 4yr Grad'
+						],
+						[
+						'option_value' => 'tr_2yr',
+						'option_text' => 'TR 2yr Grad'
+						],
+					]
+				]
+			],
+			'tabs' => [
+				[
+				'isactive' => true,
+				'label' => 'CSU Comparisons',
+				'anchor' => 'chart',
+				'embed' => 'static/page/bublin/chart.html'
+				],
+				[
+				'label' => 'Historical Trends',
+				'anchor' => 'trends',
+				'embed' => 'static/page/bublin/trends.html'
+				],
+				[
+				'label' => 'Chart Explanations',
+				'anchor' => 'explanations',
+				'embed' => 'static/page/bublin/explanations.html'
+				],
+				[
+				'label' => 'Data Tables',
+				'anchor' => 'table',
+				'embed' => 'static/page/bublin/table.html'
+				],
+				[
+				'label' => 'Methodology',
+				'anchor' => 'method',
+				'embed' => 'static/page/bublin/method.html'
+				]
+			]
+		]
     ]);
-	*/
 })->setName('test1');
 
 $app->run();
