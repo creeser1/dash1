281dcba346a3d40b7d9eba03fcd685e7ea6a1ef1
Reeser <creeser@calstate.edu>
2016-09-07 13:29:56 -0700

    towards using json to configure page template

--- src/public/index.php
+++ src/public/index.php
@@ -92,11 +92,12 @@ $app->get('/test1', function ($request, $response, $args) {
 	$this->logger->addInfo($page['htmltitle']);
 	$settings = $this->get('settings')['db'];
 	$this->logger->addInfo($settings['dbname']);
+	$newResponse = $response;
 	$jsonstring = $this->view->render($response, 'bublin-template5.html', [
         'name' => 'test1_settings.json'
     ]);
 	$this->logger->addInfo($jsonstring);
-    return $this->view->render($response, 'tpl_test1.html', [
+    return $this->view->render($newResponse, 'tpl_test1.html', [
         'page' => $page
     ]);
 })->setName('test1');
