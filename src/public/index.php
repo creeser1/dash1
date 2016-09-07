26d1b79bbf213a20f36fba45c675aac51b89bbe5
Reeser <creeser@calstate.edu>
2016-09-07 10:47:45 -0700

    added class for page construction

--- src/public/index.php
+++ src/public/index.php
@@ -88,7 +88,9 @@ $app->get('/data/{dataset}', function ($request, $response, $args) {
 
 $app->get('/test1', function ($request, $response, $args) {
 	$this->logger->addInfo("routing to /test1");
+	/*
 	$setup = new PageConfigurator('page_test1');
+	*/
 	/*
 	$this->logger->addInfo("got setup");
 	$page = $setup->getSetup();
