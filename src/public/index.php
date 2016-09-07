70cf087a1ec53ac4d9748592244f6eee506500e0
Reeser <creeser@calstate.edu>
2016-09-07 11:01:58 -0700

    revert index.php

--- src/public/index.php
+++ src/public/index.php
@@ -3,7 +3,6 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 require '../vendor/autoload.php';
-require '../vendor/settings.php';
 spl_autoload_register(function ($classname) {
     require ("../classes/" . $classname . ".php");
 });
