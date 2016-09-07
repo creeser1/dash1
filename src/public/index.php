32e9069fa87d212348120955967d15b000eed31d
Reeser <creeser@calstate.edu>
2016-09-07 10:58:33 -0700

    revert index.php

--- src/public/index.php
+++ src/public/index.php
@@ -2,19 +2,14 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-/*require '../vendor/settings.php';*/
 require '../vendor/autoload.php';
 spl_autoload_register(function ($classname) {
     require ("../classes/" . $classname . ".php");
 });
 
-/* add/overide some config items already set in required settings.php */
-$config['displayErrorDetails'] = true; 
-$settings = [
-	'settings' => $config
-];
+$config['displayErrorDetails'] = true;
 
-$app = new \Slim\App($settings);
+$app = new \Slim\App(["settings" => $config]);
 $container = $app->getContainer();
 
 $container['logger'] = function($c) {
@@ -80,73 +75,72 @@ $app->get('/bublin5', function ($request, $response, $args) {
 })->setName('bublin5');
 
 $app->get('/data/{dataset}', function ($request, $response, $args) {
-	$newResponse = $response->withHeader('Content-type', 'application/json;charset=utf-8');
+        $newResponse = $response->withHeader('Content-type', 'application/json');
     return $this->data->render($newResponse, $args['dataset'], [
         'name' => $args['dataset']
     ]);
 })->setName('dataset');
 
 $app->get('/test1', function ($request, $response, $args) {
-
     return $this->view->render($response, 'tpl_test1.html', [
         'page' => [
-			'htmltitle' => 'Test Title',
-			'title' => 'CSU Something',
-			'style' => 'static/page/bublin/all_style.css',
-			'controls' => [
-				[
-				'type' => 'select',
-				'id' => 'dataset_filter1',
-				'details' => [
-						[
-						'option_value' => 'ftf_6yr',
-						'option_text' => 'FTF 6yr Grad',
-						'isselected' => true
-						],
-						[
-						'option_value' => 'ftf_4yr',
-						'option_text' => 'FTF 4yr Grad'
-						],
-						[
-						'option_value' => 'tr_4yr',
-						'option_text' => 'TR 4yr Grad'
-						],
-						[
-						'option_value' => 'tr_2yr',
-						'option_text' => 'TR 2yr Grad'
-						],
-					]
-				]
-			],
-			'tabs' => [
-				[
-				'isactive' => true,
-				'label' => 'CSU Comparisons',
-				'anchor' => 'chart',
-				'embed' => 'static/page/bublin/chart.html'
-				],
-				[
-				'label' => 'Historical Trends',
-				'anchor' => 'trends',
-				'embed' => 'static/page/bublin/trends.html'
-				],
-				[
-				'label' => 'Chart Explanations',
-				'anchor' => 'explanations',
-				'embed' => 'static/page/bublin/explanations.html'
-				],
-				[
-				'label' => 'Data Tables',
-				'anchor' => 'table',
-				'embed' => 'static/page/bublin/table.html'
-				],
-				[
-				'label' => 'Methodology',
-				'anchor' => 'method',
-				'embed' => 'static/page/bublin/method.html'
-				]
-			]
-		]
+                        'htmltitle' => 'Test Title',
+                        'title' => 'CSU Something',
+                        'style' => 'static/page/bublin/all_style.css',
+                        'controls' => [
+                                [
+                                'type' => 'select',
+                                'id' => 'dataset_filter1',
+                                'details' => [
+                                                [
+                                                'option_value' => 'ftf_6yr',
+                                                'option_text' => 'FTF 6yr Grad',
+                                                'isselected' => true
+                                                ],
+                                                [
+                                                'option_value' => 'ftf_4yr',
+                                                'option_text' => 'FTF 4yr Grad'
+                                                ],
+                                                [
+                                                'option_value' => 'tr_4yr',
+                                                'option_text' => 'TR 4yr Grad'
+                                                ],
+                                                [
+                                                'option_value' => 'tr_2yr',
+                                                'option_text' => 'TR 2yr Grad'
+                                                ],
+                                        ]
+                                ]
+                        ],
+                        'tabs' => [
+                                [
+                                'isactive' => true,
+                                'label' => 'CSU Comparisons',
+                                'anchor' => 'chart',
+                                'embed' => 'static/page/bublin/chart.html'
+                                ],
+                                [
+                                'label' => 'Historical Trends',
+                                'anchor' => 'trends',
+                                'embed' => 'static/page/bublin/trends.html'
+                                ],
+                                [
+                                'label' => 'Chart Explanations',
+                                'anchor' => 'explanations',
+                                'embed' => 'static/page/bublin/explanations.html'
+                                ],
+                                [
+                                'label' => 'Data Tables',
+                                'anchor' => 'table',
+                                'embed' => 'static/page/bublin/table.html'
+                                ],
+                                [
+                                'label' => 'Methodology',
+                                'anchor' => 'method',
+                                'embed' => 'static/page/bublin/method.html'
+                                ]
+                        ]
+                ]
     ]);
 })->setName('test1');
 
