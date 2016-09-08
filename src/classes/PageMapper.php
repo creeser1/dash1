8676ac2f28ed9365f37527a6787b8be1f55885b6
Reeser <creeser@calstate.edu>
2016-09-08 08:46:09 -0700

    added page content database table

---
+++ src/classes/PageMapper.php
@@ -0,0 +1,56 @@
+<?php
+
+class PageMapper extends Mapper
+{
+    public function getPages() {
+        $sql = "SELECT * from page p";
+        $stmt = $this->db->query($sql);
+
+        $results = [];
+        while($row = $stmt->fetch()) {
+            $results[] = new PageEntity($row);
+        }
+        return $results;
+    }
+
+    /**
+     * Get one page by its ID
+     *
+     * @param int $page_id The ID of the ticket
+     * @return PageEntity  The page
+     */
+    public function getPageById($page_id) {
+        $sql = "SELECT * from page p
+            where p.id = :page_id";
+        $stmt = $this->db->prepare($sql);
+        $result = $stmt->execute(["page_id" => $page_id]);
+
+        if($result) {
+            return new PageEntity($stmt->fetch());
+        }
+
+    }
+
+    public function save(PageEntity $page) {
+        $sql = "insert into pgcontent
+            (type, handle, description, locator, version, status, content, editor, start) values
+            (:type, :handle, :description, :locator, :version, :status, :content, :editor, :start)"; 
+
+        $stmt = $this->db->prepare($sql);
+        $result = $stmt->execute([
+            "type" => $page->getType(),
+            "handle" => $page->getHandle(),
+            "description" => $page->getDescription(),
+            "locator" => $page->getLocator(),
+            "version" => $page->getVersion(),
+            "status" => $page->getStatus(),
+            "content" => $page->getContent(),
+            "editor" => $page->getEditor(),
+            "start" => $page->getStart(),
+        ]);
+
+        if(!$result) {
+            throw new Exception("could not save record");
+        }
+    }
+}
--- src/public/index.php
+++ src/public/index.php
@@ -48,6 +48,16 @@ $container['view'] = function ($container) {
     return $view;
 };
 
+$container['db'] = function ($c) {
+    $db = $c['settings']['db'];
+    $pdo = new PDO("mysql:host=" . $db['host'] . ";dbname=" . $db['dbname'],
+        $db['user'], $db['pass']);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+    return $pdo;
+};
+
 /*some automated server monitoring expects this*/
 $app->get('/index.html', function ($request, $response, $args) {
     return $this->view->render($response, 'root.html', [
@@ -126,5 +136,16 @@ $app->get('/test1', function ($request, $response, $args) {
         'page' => $page2
     ]);
 });
+
+$app->get('/page/{id}', function (Request $request, Response $response, $args) {
+    $page_id = (int)$args['id'];
+    $mapper = new PageMapper($this->db);
+    $page = $mapper->getPageById($page_id);
+	var_dump($page);
+	/*
+    $response = $this->view->render($response, "pageform.html", ["page" => $page]);
+	*/
+    return $response;
+})->setName('pgcontent');
 
 $app->run();
