0ccdb7ef6a3d8b8270d970903676bd870b6cbe1e
Reeser <creeser@calstate.edu>
2016-09-09 14:40:30 -0700

    made tab editable

--- src/classes/PageMapper.php
+++ src/classes/PageMapper.php
@@ -32,8 +32,8 @@ class PageMapper extends Mapper
     }
 
     public function getPageByHandle($page_handle) {
-        $sql = "SELECT * from pgcontent p
-            where p.handle = :page_handle";
+        $sql = "select * from pgcontent p
+            where p.handle = :page_handle and p.id (select max(p.id) from pgcontent where p.handle = :page_handle)";
         $stmt = $this->db->prepare($sql);
         $result = $stmt->execute(["page_handle" => $page_handle]);
 
