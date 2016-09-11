2d1e944d8ff67ec56009082860e6ca5a8518efc5
reeserc <chris.reeser@gmail.com>
2016-09-11 16:57:54 -0700

    move detail to class PageConfigurator

--- src/classes/PageConfigurator.php
+++ src/classes/PageConfigurator.php
@@ -46,6 +46,66 @@ class PageConfigurator
 		return $page;
 	}
 
+	public function loadEditor($params, $method, $dataraw) {
+
+		/*$this->logger->addInfo('--params--');
+		$this->logger->addInfo($params);
+		$this->logger->addInfo($method);
+		$this->logger->addInfo('--requested_data--');
+		$this->logger->addInfo($dataraw);*/
+		$patterns = ["/\s+/m", "/'/"];
+		$replacements = [" ", "'"];
+		$data = preg_replace($patterns, $replacements, $dataraw);
+		/*$this->logger->addInfo(preg_last_error());*/
+		/*$this->logger->addInfo($data);*/
+		/* json_decode fails if quotes not escaped in json obj values */
+		/* {"content": "<p class=\"ok\">It&apos;s ok</p>"} */
+		/* {"content": "<p class="notok">It's not ok</p>"} */
+		$json_array = json_decode($data, true);
+		/*$this->logger->addInfo(json_last_error());*/
+		/*$this->logger->addInfo('-------json_php_array---------');
+		$this->logger->addInfo(var_export($json_array, true));
+		$this->logger->addInfo('------------------');*/
+
+		/* At this point we should have the new content as a php array */
+		/* Now, get record having the latest version of this content from database */
+		/* so that it can be updated */
+
+		$tab_mapper = new PageMapper($this->db);
+		$tab_handle = $params; /*'bublin/method';*/
+		$tab_obj = $tab_mapper->getPageByHandle($tab_handle); /* latest version */
+		$tab_data = [];
+		$content = $json_array['content']; /* except that content will come from $request */
+		$description = $json_array['description']; /* and so will description */
+		$tab_data['content'] = filter_var($content, FILTER_UNSAFE_RAW); /* no weird characters */
+		$tab_data['description'] = filter_var($description, FILTER_SANITIZE_STRING); /* no html tags */
+		/*$this->logger->addInfo('------tab_data_content------');
+		$this->logger->addInfo($tab_data['content']);
+		$this->logger->addInfo('------------------');*/
+		$tab_data['type'] = $tab_obj->getType();
+		$tab_data['handle'] = $tab_obj->getHandle();
+		$tab_data['locator'] = $tab_obj->getLocator();
+		$tab_data['version'] = $tab_obj->getVersion(); /* get latest version and increment */
+		$tab_data['status'] = $tab_obj->getStatus(); /* 1,2,... or draft, published, ... */;
+		$tab_data['editor'] = $tab_obj->getEditor(); /* current authenticated username */
+		$tab_data['start'] = $tab_obj->getStart(); /* if start provided */
+		/*$this->logger->addInfo(var_export($tab_data, true));*/
+
+		/* only save if content changed */
+		if ($tab_data['content'] != $tab_obj->getContent()) {
+			$tab = new PageEntity($tab_data); /* create new PageEntity object from array */
+			$tab_mapper->save($tab);
+		}
+
+		/* Finally, respond with accepted json object */
+		$json = json_encode($tab_data);
+		$this->logger->addInfo('--json response--');
+		$this->logger->addInfo($json);
+		$this->logger->addInfo('------------------');
+
+		return $tab_data;
+	}
+
 	public function getSetup() { /* eventually replace this inline code by reading JSON from stream */
 		$page = [
 			'htmltitle' => 'Test Title',
--- src/public/index.php
+++ src/public/index.php
@@ -98,15 +98,6 @@ $app->get('/data/{dataset}', function ($request, $response, $args) {
     ]);
 });
 
-$app->get('/test2', function ($request, $response, $args) {
-	$this->logger->addInfo('test2');
-	$newResponse = $response->withHeader('Content-type', 'application/json');
-	$this->logger->addInfo('json test2');
-    return $this->data->render($newResponse, 'test1_settings.json', [
-        'name' => 'test1_settings.json'
-    ]);
-});
-
 $app->get('/page/{id}', function ($request, $response, $args) {
 	$builder = new PageConfigurator('bublin', $this->db);
 	$page = $builder->loadPage($args['id']);
@@ -118,39 +109,8 @@ $app->get('/page/{id}', function ($request, $response, $args) {
 });
 
 $app->get('/edit/{id}', function ($request, $response, $args) {
-	$settings = $this->get('settings')['db'];
-  $mapper = new PageMapper($this->db);
-	$page_id = (int)$args['id'];
-
-	/* get the json config for requested page id */
-  $page_settings = $mapper->getPageById($page_id);
-	$json = $page_settings->getContent();
-	/*$this->logger->addInfo($json);*/
-	$page = json_decode($json, true);
-	$json_err = json_last_error();
-	if ($json_err != 0) {
-	  $this->logger->addInfo($json_err);
-  } /* else */
-  	/* assert $page has at least one of tabs and each has content */
-  if (is_array($page) and array_key_exists('tabs', $page)
-  		and is_array($page['tabs']) and is_array($page['tabs'][0])) {
-		$index = 0;
-		$this->logger->addInfo('============');
-		foreach ($page['tabs'] as $tab) {
-			/*load the tab content and place in tempate variables, eventually using query returning all at once*/
-			$page_handle = $tab['embed']; /*'bublin/explanations';*/
-			$this->logger->addInfo($page_handle);
-			$tab_content = $mapper->getPageByHandle($page_handle);
-			$html = $tab_content->getContent();
-			$id = $tab_content->getId();
-			/*$this->logger->addInfo($id);
-			$this->logger->addInfo($html);
-			$this->logger->addInfo('============');*/
-			$page['tabs'][$index]['content'] = stripslashes($html); /* previously added to double quotes */
-			/* $this->logger->addInfo(var_export($page, true)); */
-			$index = $index + 1;
-		}
-	}
+	$builder = new PageConfigurator('bublin', $this->db);
+	$page = $builder->loadPage($args['id']);
 
     return $this->view->render($response, 'tpl_test1.html', [
         'page' => $page
@@ -162,70 +122,18 @@ $app->get('/dump/{id}', function (Request $request, Response $response, $args) {
   $mapper = new PageMapper($this->db);
   $page = $mapper->getPageById($page_id);
 	var_dump($page);
-	/*
-    $response = $this->view->render($response, "pageform.html", ["page" => $page]);
-	*/
+
   return $response;
-})->setName('pgcontent');
+});
 
 $app->map(['PUT', 'POST'], '/tab[/{params:.*}]', function (Request $request, Response $response, $args) {
 	$dataraw = $request->getBody();
 	$params = $request->getAttribute('params');
 	$method = $request->getMethod();
-	$this->logger->addInfo('--params--');
-	$this->logger->addInfo($params);
-	$this->logger->addInfo($method);
-	$this->logger->addInfo('--requested_data--');
-	$this->logger->addInfo($dataraw);
-	$patterns = ["/\s+/m", "/'/"];
-	$replacements = [" ", "'"];
-	$data = preg_replace($patterns, $replacements, $dataraw);
-	$this->logger->addInfo(preg_last_error());
-	/*$this->logger->addInfo($data);*/
-	/* json_decode fails if quotes not escaped in json obj values */
-	/* {"content": "<p class=\"ok\">It&apos;s ok</p>"} */
-	/* {"content": "<p class="notok">It's not ok</p>"} */
-	$json_array = json_decode($data, true);
-  $this->logger->addInfo(json_last_error());
-	/*$this->logger->addInfo('-------json_php_array---------');
-  $this->logger->addInfo(var_export($json_array, true));
-	$this->logger->addInfo('------------------');*/
-
-	/* At this point we should have the new content as a php array */
-	/* Now, get record having the latest version of this content from database */
-	/* so that it can be updated */
-
-	$tab_mapper = new PageMapper($this->db);
-	$tab_handle = $params; /*'bublin/method';*/
-	$tab_obj = $tab_mapper->getPageByHandle($tab_handle); /* latest version */
-	$tab_data = [];
-	$content = $json_array['content']; /* except that content will come from $request */
-	$description = $json_array['description']; /* and so will description */
-	$tab_data['content'] = filter_var($content, FILTER_UNSAFE_RAW); /* no weird characters */
-	$tab_data['description'] = filter_var($description, FILTER_SANITIZE_STRING); /* no html tags */
-	/*$this->logger->addInfo('------tab_data_content------');
-	$this->logger->addInfo($tab_data['content']);
-	$this->logger->addInfo('------------------');*/
-	$tab_data['type'] = $tab_obj->getType();
-	$tab_data['handle'] = $tab_obj->getHandle();
-	$tab_data['locator'] = $tab_obj->getLocator();
-	$tab_data['version'] = $tab_obj->getVersion(); /* get latest version and increment */
-	$tab_data['status'] = $tab_obj->getStatus(); /* 1,2,... or draft, published, ... */;
-	$tab_data['editor'] = $tab_obj->getEditor(); /* current authenticated username */
-	$tab_data['start'] = $tab_obj->getStart(); /* if start provided */
-  /*$this->logger->addInfo(var_export($tab_data, true));*/
-
-	/* only save if content changed */
-	if ($tab_data['content'] != $tab_obj->getContent()) {
-		$tab = new PageEntity($tab_data); /* create new PageEntity object from array */
-		$tab_mapper->save($tab);
-	}
-
-	/* Finally, respond with accepted json object */
-	$json = json_encode($tab_data);
-  $this->logger->addInfo('--json response--');
-  $this->logger->addInfo($json);
-	$this->logger->addInfo('------------------');
+	
+	$builder = new PageConfigurator('bublin', $this->db);
+	$tab_data = $builder->loadEditor($params, $method, $dataraw);
+	
 	$newResponse = $response->withHeader('Content-type', 'application/json');
 	$jsonResponse = $newResponse->withJson($tab_data);
 
