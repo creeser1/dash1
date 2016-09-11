f4178c07e97ad7d1480049bd70bc898f30be5c56
reeserc <chris.reeser@gmail.com>
2016-09-11 09:06:54 -0700

    escape json content

--- src/public/index.php
+++ src/public/index.php
@@ -155,9 +155,9 @@ $app->map(['PUT', 'POST'], '/tab[/{params:.*}]', function (Request $request, Res
 	/*$data = addslashes($data);*/
 	$this->logger->addInfo($data);
 	$json_array = json_decode($data, true);
+  $this->logger->addInfo(json_last_error());
 	$this->logger->addInfo('-------json_php_array---------');
-  	$this->logger->addInfo(var_export($json_array, true));
-  	$this->logger->addInfo(json_last_error());
+  $this->logger->addInfo(var_export($json_array, true));
 	$this->logger->addInfo('------------------');
 	$tab_mapper = new PageMapper($this->db);
 	$tab_handle = 'bublin/method';
