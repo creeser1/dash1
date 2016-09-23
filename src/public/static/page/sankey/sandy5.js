1c2200902ad78433cd9a88446875189cdb13bd93
Reeser <creeser@calstate.edu>
2016-09-23 15:07:28 -0700

    refactored edit templating

--- src/public/static/page/sankey/sandy5.js
+++ src/public/static/page/sankey/sandy5.js
@@ -130,7 +130,6 @@
 		return [sankeydata, pivot, totalfrom, totalto, totalto + totalfrom];
 	};
 
-
 	var load_data = function (config, callback) {
 		if (datacache.hasOwnProperty(config.data_url)) {
 			callback(datacache[config.data_url], config);
@@ -151,217 +150,198 @@
 
 	var create_campus_list = function (callback) {
 		var config = {'data_url': '/data/sankey/data/migrationsjson/crr_campuses_by_name.json'};
-		load_data(config, function (result, config) {
-			var out = {};
-			Object.keys(result).forEach(function (key) {
-				out[result[key]] = key;
-			});
-			callback(Object.keys(result), config);
+		load_data(config, function (result) {
+			callback(Object.keys(result));
 		});
 	};
 
 	var get_major_map = function (campus, callback) {
 		var config = {'data_url': '/data/sankey/data/migrationsjson/crr_majorcode2desc.json'};
-		load_data(config, function (result, config) {
-			callback(result[campus], config);
+		load_data(config, function (result) {
+			callback(result[campus]);
 		});
 	};
 
 	var get_college_map = function (campus, callback) {
 		var config = {'data_url': '/data/sankey/data/migrationsjson/crr_colleges_by_major_code.json'};
-		if (cs.retained_data) {
-			callback(cs.retained_data[campus], config);
-			return;
-		}
-		load_data(config, function (result, config) {
-			cs.retained_data = result;
-			callback(result[campus], config);
+		load_data(config, function (result) {
+			callback(result[campus]);
 		});
 	};
 	
 	var get_migrations = function (campus, callback) {
 		var config = {'data_url': '/data/sankey/data/migrationsjson/crr_migration_col_ftf.json'};
-		load_data(config, function (result, config) {
-			callback(result[campus], config);
+		load_data(config, function (result) {
+			callback(result[campus]);
 		});
 	};
 
-	var create_college_selector = function (college_map) {
-		var optc = [];
-		var out1 = '';
-		var selected_college;
-		cs.college_map = college_map;
-		Object.keys(college_map).forEach(function (el) {
-			optc.push(college_map[el]);
-		});
-		optc.sort();
-		optc = optc.filter(function (el, i, a) {return i < 0 || el !== a[i - 1];}); // distinct list
-		optc.forEach(function (el) {
-			var selected = '';
-			if (el === cs.filter_college) {
-				selected = 'selected';
-				selected_college = el;
+	var create_selector = function (list, selected) {
+		var options = list.slice();
+		options.sort();
+		options = _.uniq(options, true);
+		var template = '<option value="{v}"{s}>{t}</option>';
+		var out = _.map(options, function (el) {
+			var tpl = template.replace('{v}', el).replace('{t}', el);
+			if (el === selected) {
+				return tpl.replace('{s}', ' selected');
+			} else {
+				return tpl.replace('{s}', '');
 			}
-			out1 += '<option value="{v0}" {sel}>{v1}</option>'.replace(/{v\d}/g, el).replace('{sel}', selected);
 		});
-		$('#fromselector').html(out1);
-		return selected_college;
+		return out.join('');
 	};
-
-	var create_major_selector = function (major_map, college_map, selected_college) {
-		var optm = [];
-		var out2 = [];
-		var selected_major;
-		cs.major_map = major_map;
-		Object.keys(major_map).forEach(function (el, i, a) {
-			if (college_map[el] === selected_college) {
-				optm.push(major_map[el]);
-			}
-		});
-		optm.sort();
-		optm = optm.filter(function (el, i, a) {return i < 0 || el !== a[i - 1];}); // distinct list
-		optm.forEach(function (el) {
-			var selected = '';
-			if (el === cs.filter_major) {
-				selected = 'selected';
-				selected_major = el;
-			}
-			out2.push('<option value="{v0}" {sel}>{v1}</option>'.replace(/{v\d}/g, el).replace('{sel}', selected));
-		});
-		if (!selected_major) {
-			out2[0] = '<option value="{v0}" selected>{v1}</option>'.replace(/{v\d}/g, optm[0]);
-			cs.filter_major = optm[0];
+	
+	var create_college_selector = function (list, selected) {
+		var sel = selected;
+		if (!_.contains(list, selected)) {
+			sel = list[0];
 		}
-		$('#toselector').html(out2.join(''));
-		return selected_major;
+		$('#fromselector').html(create_selector(list, sel));
+		return sel;
 	};
 
-	var create_fromtoboth_selector = function (hasfrom, hasto) {
-		var result3;
-		var hasto = false;
-		var hasfrom = false;
-		var filter_campus = cs.filter_campus;
-		var out3 = [];
-		get_migrations(filter_campus, function (result3, config) {
-			var enrolled_majors = Object.keys(result3.enrolled);
-			var graduation_majors = Object.keys(result3.graduation);
-			enrolled_majors.forEach(function (code) {
-				var item = result3.enrolled[code];
-				item.forEach(function (major) {
-					if (cs.major_map[major[1]] === cs.filter_major && cs.college_map[major[1]] === cs.filter_college) {
-						hasfrom = true;
+	var create_major_selector = function (list, selected) {
+		var sel = selected;
+		if (!_.contains(list, selected)) {
+			sel = list[0];
+		}
+		$('#toselector').html(create_selector(list, sel));
+		return sel;
+	};
+
+	var create_migration_selector = function (list, selected) {
+		var sel = selected;
+		if (!_.contains(list, selected)) {
+			sel = list[0];
+		}
+		$('#fromtobothselector').html(create_selector(list, sel));
+		return sel;
+	};
+
+	var config_controls = function (callback) { // only load data once, but reconfigure cascading controls as needed
+		//cs.filter_campus, cs.filter_college, cs.filter_major
+		// load the map of colleges by major code for a given campus
+		get_college_map(cs.filter_campus, function (college_map) {
+			// given a campus, populate dropdown with college_of values
+			if (_.size(college_map)) {
+				// populate the dropdown and return the item selected (same as before change of campus if possible)
+				var college_list = _.toArray(college_map);
+				console.log(JSON.stringify(college_list));
+				var selected_college = create_college_selector(college_list, cs.filter_college);
+				cs.filter_college = selected_college;
+				// given a campus and college_of at that campus, populate dropdown with majors
+				get_major_map(cs.filter_campus, function (major_map) {
+					var major_code_list = _.filter(Object.keys(major_map), function (key) {
+						return (college_map[key] === selected_college);
+					});
+					console.log(JSON.stringify(major_code_list)); // the major codes
+					var major_name_list = _.filter(major_map, function (val, key) {
+						return (college_map[key] === selected_college);
+					});
+					console.log(JSON.stringify(major_name_list)); // the major codes
+					var selected_major = create_major_selector(major_name_list, cs.filter_major);
+					cs.filter_major = selected_major;
+					if (_.size(major_name_list)) {
+						get_migrations(cs.filter_campus, function (migrations) {
+							var option_list = [];
+							if (_.size(migrations['enrolled'])) {
+								option_list.push('From Only');
+								if (_.size(migrations['graduation'])) {
+									option_list.push('To Only');
+									option_list.push('Both From and To');
+								}
+							} else if (_.size(migrations['graduation'])) {
+								option_list.push('To Only');
+							}
+							var selected_migrations = create_migrations_selector(option_list, cs.filter_migrations);
+							cs.filter_migrations = selected_migrations;
+							console.log(JSON.stringify(migrations));
+							if (callback) {
+								callback(college_map, major_map, migrations);
+							}
+						});
 					}
 				});
-			});
-			graduation_majors.forEach(function (code) {
-				var item = result3.graduation[code];
-				item.forEach(function (major) {
-					if (cs.major_map[major[0]] === cs.filter_major && cs.college_map[major[0]] === cs.filter_college) {
-						hasto = true;
-					}
-				});
-			});
-			var template = '<option value="{v}">{t}</option>';
-			if (hasfrom && hasto) {
-				out3.push(template.replace('{v}', 'both').replace('{t}', 'Both From and To'));
 			}
-			if (hasfrom) {
-				out3.push(template.replace('{v}', 'from').replace('{t}', 'From Only'));
-			}
-			if (hasto) {
-				out3.push(template.replace('{v}', 'to').replace('{t}', 'To Only'));
-			}
-			$('#fromtobothselector').html(out3.join(''));
 		});
 	};
 
-	var config_chart = function (filter_campus, filter_college, filter_major, callback) { // initially and on change of campus
+	var config_chart = function (college_map, major_map, migrations, callback) { // initially and on change of campus
 		log = {'value': '', 'listto': [], 'listfrom': [], 'pivot': null, 'list': []};
 
-		create_campus_list(function (result, config) {
-		});
-
-		get_college_map(filter_campus, function (college_map, config) {
-			var selected_college = create_college_selector(college_map); // once per campus
-			get_major_map(filter_campus, function (major_map, config) { // initially and on change of college
-				var selected_major = create_major_selector(major_map, college_map, selected_college);
-
-				var pivot = null;
-				var listfrom = [];
-				var listto = [];
-				get_migrations(filter_campus, function (result3, config) {
-					var enrolled_majors = Object.keys(result3.enrolled);
-					var graduation_majors = Object.keys(result3.graduation);
-					enrolled_majors.forEach(function (code) {
-						var item = result3.enrolled[code];
-						item.forEach(function (major) {
-							if (major_map[major[1]] === cs.filter_major && college_map[major[1]] === cs.filter_college) {
-								if (major_map[major[1]] === major_map[major[0]]) {
-									pivot = {'Source': major_map[major[1]], 'Destination': major_map[major[0]], 'Students': parseInt(major[2],10), 'Formatted': '%', 'Flow': 'to'};
-								} else {
-									listfrom.push({'Source': major_map[major[1]], 'Destination': major_map[major[0]], 'Students': parseInt(major[2],10), 'Formatted': '%', 'Flow': 'from'});
-								}
-							}
-						});
-					});
-					graduation_majors.forEach(function (code) {
-						var item = result3.graduation[code];
-						item.forEach(function (major) {
-							if (major_map[major[0]] === cs.filter_major && college_map[major[0]] === cs.filter_college) {
-								if (major_map[major[1]] !== major_map[major[0]]) {
-									listto.push({'Source': major_map[major[1]], 'Destination': major_map[major[0]], 'Students': parseInt(major[2],10), 'Formatted': '%', 'Flow': 'to'});
-								} else if (pivot === null) {
-									pivot = {'Source': major_map[major[1]], 'Destination': major_map[major[0]], 'Students': parseInt(major[2],10), 'Formatted': '%', 'Flow': 'to'};
-								}
-							}
-						});
-					});
-
-					var list = [];
-					var studentsort1 = function (a, b) {
-						var cmp = parseInt(b.Students, 10) - parseInt(a.Students, 10);
-						if (cmp === 0) {
-							if (a.Source === b.Source) {
-								return 0;
-							} else if (a.Source > b.Source) {
-								return 1;
-							} else {
-								return -1;
-							}
-						}
-						return cmp;
-					};
-					var studentsort2 = function (a, b) {
-						var cmp = parseInt(b.Students, 10) - parseInt(a.Students, 10);
-						if (cmp === 0) {
-							if (a.Destination === b.Destination) {
-								return 0;
-							} else if (a.Destination > b.Destination) {
-								return 1;
-							} else {
-								return -1;
-							}
-						}
-						return cmp;
-					};
-					if (!!listto) {
-						listto.sort(studentsort1);
-						list = list.concat(listto);
+		var pivot = null;
+		var listfrom = [];
+		var listto = [];
+		var enrolled_majors = Object.keys(migrations.enrolled);
+		var graduation_majors = Object.keys(migrations.graduation);
+		enrolled_majors.forEach(function (code) {
+			var item = migrations.enrolled[code];
+			item.forEach(function (major) {
+				if (major_map[major[1]] === cs.filter_major && college_map[major[1]] === cs.filter_college) {
+					if (major_map[major[1]] === major_map[major[0]]) {
+						pivot = {'Source': major_map[major[1]], 'Destination': major_map[major[0]], 'Students': parseInt(major[2],10), 'Formatted': '%', 'Flow': 'to'};
+					} else {
+						listfrom.push({'Source': major_map[major[1]], 'Destination': major_map[major[0]], 'Students': parseInt(major[2],10), 'Formatted': '%', 'Flow': 'from'});
 					}
-					if (!!pivot) {
-						list = list.concat(pivot);
-					}
-					if (!!listfrom) {
-						listfrom.sort(studentsort2);
-						list = list.concat(listfrom);
-					}
-
-					log.list = list;
-					var results = ingest(log.list, threshold);
-					callback([results, log.list]);
-				});
+				}
 			});
 		});
+		graduation_majors.forEach(function (code) {
+			var item = migrations.graduation[code];
+			item.forEach(function (major) {
+				if (major_map[major[0]] === cs.filter_major && college_map[major[0]] === cs.filter_college) {
+					if (major_map[major[1]] !== major_map[major[0]]) {
+						listto.push({'Source': major_map[major[1]], 'Destination': major_map[major[0]], 'Students': parseInt(major[2],10), 'Formatted': '%', 'Flow': 'to'});
+					} else if (pivot === null) {
+						pivot = {'Source': major_map[major[1]], 'Destination': major_map[major[0]], 'Students': parseInt(major[2],10), 'Formatted': '%', 'Flow': 'to'};
+					}
+				}
+			});
+		});
+
+		var list = [];
+		var studentsort1 = function (a, b) {
+			var cmp = parseInt(b.Students, 10) - parseInt(a.Students, 10);
+			if (cmp === 0) {
+				if (a.Source === b.Source) {
+					return 0;
+				} else if (a.Source > b.Source) {
+					return 1;
+				} else {
+					return -1;
+				}
+			}
+			return cmp;
+		};
+		var studentsort2 = function (a, b) {
+			var cmp = parseInt(b.Students, 10) - parseInt(a.Students, 10);
+			if (cmp === 0) {
+				if (a.Destination === b.Destination) {
+					return 0;
+				} else if (a.Destination > b.Destination) {
+					return 1;
+				} else {
+					return -1;
+				}
+			}
+			return cmp;
+		};
+		if (!!listto) {
+			listto.sort(studentsort1);
+			list = list.concat(listto);
+		}
+		if (!!pivot) {
+			list = list.concat(pivot);
+		}
+		if (!!listfrom) {
+			listfrom.sort(studentsort2);
+			list = list.concat(listfrom);
+		}
+
+		log.list = list;
+		var results = ingest(log.list, threshold);
+		callback([results, log.list]);
 	};
 
 	var build_table = function (data) {
@@ -402,18 +382,19 @@
 			campusname = e.target.value;
 			cs.filter_campus = campusname;
 			//create_college_selector(cs.major_map, cs.college_map, fromcode);
+			config_controls();
 		});
 
 		$('#fromselector').on('change', function (e) {
 			fromcode = e.target.value;
 			cs.filter_college = fromcode;
-			create_major_selector(cs.major_map, cs.college_map, fromcode);
+			config_controls();
 		});
 			
 		$('#toselector').on('change', function (e) {
 			tocode = e.target.value;
 			cs.filter_major = tocode;
-			create_fromtoboth_selector();
+			config_controls();
 		});
 
 		$('#fromtobothselector').on('change', function (e) {
@@ -421,23 +402,26 @@
 		});
 
 		// create the chart initially, using default filters
-		config_chart(cs.filter_campus, cs.filter_college, cs.filter_major, function (chart_config) {
-			$('#table').empty();
-			$('<div id="migration_table">' + build_table(chart_config[1]) + '</div>').appendTo('#table');
-			$('body').trigger('create_chart', {'chart_config': chart_config[0]});
+		config_controls(function (campus_map, majors_map, migrations) {
+			config_chart(college_map, major_map, migrations, function (chart_config) {
+				$('#table').empty();
+				$('<div id="migration_table">' + build_table(chart_config[1]) + '</div>').appendTo('#table');
+				$('body').trigger('create_chart', {'chart_config': chart_config[0]});
+			});
 		});
 
 		// create the chart on request, using current filter settings
 		$('#gobtn').on('click', function (e) {
 			e.preventDefault();
 			e.stopPropagation();
-			config_chart(cs.filter_campus, cs.filter_college, cs.filter_major, function (chart_config) {
-				$('#table').empty();
-				$('<div id="migration_table">' + build_table(chart_config[1]) + '</div>').appendTo('#table');
-				$('body').trigger('create_chart', {'chart_config': chart_config[0]});
+			config_controls(function (campus_map, majors_map, migrations) {
+				config_chart(college_map, major_map, migrations, function (chart_config) {
+					$('#table').empty();
+					$('<div id="migration_table">' + build_table(chart_config[1]) + '</div>').appendTo('#table');
+					$('body').trigger('create_chart', {'chart_config': chart_config[0]});
+				});
 			});
 		});
-		//$('#gobtn').click();
 	};
 	init();
 }());
