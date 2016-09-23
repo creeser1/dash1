5b614e4958faf3937d9578466fc854e57205bf15
Reeser <creeser@calstate.edu>
2016-09-23 10:00:57 -0700

    refactored edit templating

--- src/public/static/page/sankey/sandy4.js
+++ src/public/static/page/sankey/sandy4.js
@@ -2,14 +2,21 @@
 	'use strict';
 
 	var units = "Students";
-	var palette = ['#aa6', '#f90', '#d6a', '#a6d', '#f0f', '#6da', '#ad6', '#a66', '#a6a', '#66a', '#6ad', '#0dc', '#b3a', '#0df', '#6a0', '#f3a', '#6a6', '#6aa', '#da6', '#00f', '#0af', '#f00', '#0f3', '#60f', '#fe0', '#06a'];
+	var palette = ['#aa6', '#f90', '#d6a', '#a6d', '#f0f', '#6da', '#a66', '#a6a', '#6ad', '#0dc', '#b3a', '#0df', '#6a0', '#f3a', '#6a6', '#6aa', '#da6', '#00f', '#0af', '#f00', '#0f3', '#60f', '#fe0', '#06a', '#66a', '#ad6', '#aaa'];
 	var svg;
 	
 	// a helper function for consistently coloring nodes by using a hashing of their text label
-	var hasher = function (str) {
+	var hasher = function (str, pivot) {
+		var cycle = palette.length - 3; // last 3 are reserved for [pivot, undeclared, other]
+		if (str === pivot) {
+			return cycle + 0;
+		} else if (str === 'Undeclared') {
+			return cycle + 1;
+		} else if (str === 'Other') {
+			return cycle + 2;
+		}
 		var offset = 104;
 		var slope = 17.0 / 7.0;
-		var cycle = palette.length;
 		var normal = str.toUpperCase().replace(/^[A-Z]/g, '');
 		var scores = [];
 		var score = 0;
@@ -115,16 +122,16 @@
 			.attr("d", path)
 			.style('fill', 'transparent') 
 			.style('stroke', function (d) { 
-				var c = color(d.target.name === base ? hasher(d.source.name) : hasher(d.target.name)); // by name hash
+				var c = color(d.target.name === base ? hasher(d.source.name, base) : hasher(d.target.name, base)); // by name hash
 				return d3.rgb(c);
 			})
 			.on("mouseover", function (d) {
 				console.log(d);
-				var c = color(d.target.name === base ? hasher(d.source.name) : hasher(d.target.name)); // by name hash
+				var c = color(d.target.name === base ? hasher(d.source.name, base) : hasher(d.target.name, base)); // by name hash
 				this.style.stroke = d3.rgb(c).darker(1);
 			})
 			.on("mouseout", function (d) {
-				var c = color(d.target.name === base ? hasher(d.source.name) : hasher(d.target.name)); // by name hash
+				var c = color(d.target.name === base ? hasher(d.source.name, base) : hasher(d.target.name, base)); // by name hash
 				this.style.stroke = d3.rgb(c);
 			})
 				.style("stroke-width", function(d) {
